db277b3216ff79e6950733f556111c85125f4674
silentwolf-dev <omariimanibrown421@gmail.com>
2021-10-15 21:25:39 -0400

    new changes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,19 @@
-interface commandInterface {
+interface CommandLibraryInterface {
     name?: string;
     trigger?: string;
     description?: string;
     execute?: any;
 }
-export declare function initializeCommand(trigger: string, commands: [commandInterface]): Error | undefined;
+export declare function initializeCommand(trigger: string, commands: [CommandLibraryInterface]): Error | undefined;
 export declare class Command {
-    constructor(commandObject: commandInterface);
+    constructor(commandObject: CommandLibrary);
 }
 export declare class CommandLibrary {
-    constructor(commands: [commandInterface]);
+    constructor(commands: [CommandLibraryInterface]);
 }
 export declare class Executer {
-    commands: [commandInterface] | undefined | null;
-    constructor(commands: [commandInterface]);
+    commands: [CommandLibraryInterface] | undefined | null;
+    constructor(commands: [CommandLibraryInterface]);
     initializeCommand(trigger: string): void;
 }
 export {};
